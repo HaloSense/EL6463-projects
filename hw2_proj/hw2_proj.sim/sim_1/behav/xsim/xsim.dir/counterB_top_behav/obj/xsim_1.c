bf7bff7bc5c7d3c24fd61d28c1fff18d7cf4ad20
HaloSense <liuzj990412@live.com>
2022-09-24 23:26:22 -0400

    some files

--- hw2_proj/hw2_proj.sim/sim_1/behav/xsim/xsim.dir/counterB_top_behav/obj/xsim_1.c
+++ hw2_proj/hw2_proj.sim/sim_1/behav/xsim/xsim.dir/counterB_top_behav/obj/xsim_1.c
@@ -54,41 +54,34 @@
 #endif
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
-IKI_DLLESPEC extern void execute_2(char*, char *);
-IKI_DLLESPEC extern void execute_3(char*, char *);
-IKI_DLLESPEC extern void execute_12(char*, char *);
-IKI_DLLESPEC extern void execute_13(char*, char *);
-IKI_DLLESPEC extern void execute_5(char*, char *);
-IKI_DLLESPEC extern void execute_6(char*, char *);
-IKI_DLLESPEC extern void execute_8(char*, char *);
-IKI_DLLESPEC extern void execute_9(char*, char *);
-IKI_DLLESPEC extern void execute_10(char*, char *);
-IKI_DLLESPEC extern void execute_11(char*, char *);
-IKI_DLLESPEC extern void execute_14(char*, char *);
-IKI_DLLESPEC extern void execute_15(char*, char *);
-IKI_DLLESPEC extern void execute_16(char*, char *);
-IKI_DLLESPEC extern void execute_17(char*, char *);
-IKI_DLLESPEC extern void execute_18(char*, char *);
-IKI_DLLESPEC extern void execute_19(char*, char *);
-IKI_DLLESPEC extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[17] = {(funcp)execute_2, (funcp)execute_3, (funcp)execute_12, (funcp)execute_13, (funcp)execute_5, (funcp)execute_6, (funcp)execute_8, (funcp)execute_9, (funcp)execute_10, (funcp)execute_11, (funcp)execute_14, (funcp)execute_15, (funcp)execute_16, (funcp)execute_17, (funcp)execute_18, (funcp)execute_19, (funcp)vlog_transfunc_eventcallback};
-const int NumRelocateId= 17;
+IKI_DLLESPEC extern void execute_24(char*, char *);
+IKI_DLLESPEC extern void execute_25(char*, char *);
+IKI_DLLESPEC extern void execute_27(char*, char *);
+IKI_DLLESPEC extern void execute_28(char*, char *);
+IKI_DLLESPEC extern void transaction_0(char*, char*, unsigned, unsigned, unsigned);
+IKI_DLLESPEC extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
+IKI_DLLESPEC extern void transaction_3(char*, char*, unsigned, unsigned, unsigned);
+funcp funcTab[7] = {(funcp)execute_24, (funcp)execute_25, (funcp)execute_27, (funcp)execute_28, (funcp)transaction_0, (funcp)vhdl_transfunc_eventcallback, (funcp)transaction_3};
+const int NumRelocateId= 7;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/counterA_top_behav/xsim.reloc",  (void **)funcTab, 17);
+	iki_relocate(dp, "xsim.dir/counterB_top_behav/xsim.reloc",  (void **)funcTab, 7);
+	iki_vhdl_file_variable_register(dp + 3048);
+	iki_vhdl_file_variable_register(dp + 3104);
+
 
 	/*Populate the transaction function pointer field in the whole net structure */
 }
 
 void sensitize(char *dp)
 {
-	iki_sensitize(dp, "xsim.dir/counterA_top_behav/xsim.reloc");
+	iki_sensitize(dp, "xsim.dir/counterB_top_behav/xsim.reloc");
 }
 
 void simulate(char *dp)
 {
-		iki_schedule_processes_at_time_zero(dp, "xsim.dir/counterA_top_behav/xsim.reloc");
+		iki_schedule_processes_at_time_zero(dp, "xsim.dir/counterB_top_behav/xsim.reloc");
 	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
 	iki_execute_processes();
 
@@ -110,9 +103,9 @@ extern SYSTEMCLIB_IMP_DLLSPEC char** xsim_argv_copy ;
 int main(int argc, char **argv)
 {
     iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
-    iki_set_sv_type_file_path_name("xsim.dir/counterA_top_behav/xsim.svtype");
-    iki_set_crvs_dump_file_path_name("xsim.dir/counterA_top_behav/xsim.crvsdump");
-    void* design_handle = iki_create_design("xsim.dir/counterA_top_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, (void*)0, 0, isimBridge_getWdbWriter(), 0, argc, argv);
+    iki_set_sv_type_file_path_name("xsim.dir/counterB_top_behav/xsim.svtype");
+    iki_set_crvs_dump_file_path_name("xsim.dir/counterB_top_behav/xsim.crvsdump");
+    void* design_handle = iki_create_design("xsim.dir/counterB_top_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, (void*)0, 0, isimBridge_getWdbWriter(), 0, argc, argv);
      iki_set_rc_trial_count(100);
     (void) design_handle;
     return iki_simulate_design();
---
+++ hw2_proj/script.md
@@ -0,0 +1,13 @@
+# Video script
+
+## Counter A
+
+I will now demonstrate the functions of counter A. The reset switch is switch 0 and the enable switch is switch 1.
+
+When reset is 1, the counter value is set to 1, the first digit of my N number. When reset is 0 and enable is 1, the counter starts counting up. When enable is 0, the counter stops. Whenever reset turns 1, the counter is set back to 1.
+
+## Counter B
+
+This is the second counter B.
+
+When reset is 1, the counter value is set to 3, the last digit of my N number. When reset is 0 and enable is 1, the counter starts counting down. When enable is 0, the counter stops. Whenever reset turns 1, the counter is set back to 3.
